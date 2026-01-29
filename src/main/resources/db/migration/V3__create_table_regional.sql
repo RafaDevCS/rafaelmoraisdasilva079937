192da0705b595c6c8a402dafe5ba3eb60e4a5c0b
Rafael Morais da Silva <rafaelmorais@sad.infovia-mt>
2026-01-29 17:00:07 -0400

    Import da lista para tabela regionais

--- src/main/resources/db/migration/V3__create_table_regional.sql
+++ src/main/resources/db/migration/V3__create_table_regional.sql
@@ -6,8 +6,42 @@ CREATE TABLE regional (
 
 ALTER TABLE artist ADD COLUMN regional_id INTEGER REFERENCES regional(id);
 
--- Inserindo dados de exemplo para bater com o formato esperado
-INSERT INTO regional (nome, ativo) VALUES ('Sul', true);
-INSERT INTO regional (nome, ativo) VALUES ('Sudeste', true);
-INSERT INTO regional (nome, ativo) VALUES ('Nordeste', true);
+INSERT INTO regional (id, nome, ativo) VALUES 
+(9, 'REGIONAL DE CUIABÁ', true),
+(31, 'REGIONAL DE GUARANTÃ DO NORTE', true),
+(32, 'REGIONAL DE VILA RICA', true),
+(39, 'LDAPREGIONAL', true),
+(27, 'DGPJCM - SEDE', true),
+(42, 'REGIONALDAP01', true),
+(43, 'REGIONALLDAP02', true),
+(11, 'REGIONAL DE AGUA BOA', true),
+(13, 'REGIONAL DE ALTO ARAGUAIA', true),
+(14, 'REGIONAL DE BARRA DO GARÇAS', true),
+(15, 'REGIONAL DE CÁCERES', true),
+(10, 'REGIONAL DE VÁRZEA GRANDE', true),
+(12, 'REGIONAL DE ALTA FLORESTA', true),
+(16, 'REGIONAL DE NOVA MUTUM', true),
+(17, 'REGIONAL DE JUÍNA', true),
+(18, 'REGIONAL DE PONTES E LACERDA', true),
+(19, 'REGIONAL DE CONFRESA', true),
+(20, 'REGIONAL DE RONDONÓPOLIS', true),
+(21, 'REGIONAL DE SINOP', true),
+(22, 'REGIONAL DE TANGARÁ DA SERRA', true),
+(29, 'REGIONAL DE PRIMAVERA DO LESTE', true),
+(30, 'REGIONAL DE PEIXOTO DE AZEVEDO', true),
+(28, 'COORDENADORIA DE POLÍCIA COMUNITÁRIA', true),
+(33, 'COORDENADORIA METROPOLITANA DE PLANTÕES POLICIAIS', true),
+(2, ' NÃO SE APLICA - CORREGEPOL', true),
+(4, ' NÃO SE APLICA - DAE', true),
+(5, ' NÃO SE APLICA - DEE', true),
+(6, ' NÃO SE APLICA - DI', true),
+(7, ' NÃO SE APLICA - DGPJCA', true),
+(8, ' NÃO SE APLICA - OUVIDORIA', true),
+(23, ' NÃO SE APLICA - DGPJC', true),
+(1, ' NÃO SE APLICA - ACADEPOL', true),
+(26, ' DGPJCI - SEDE', true)
+ON CONFLICT (id) DO NOTHING;
+
+-- evita erro de duplicidade
+SELECT setval(pg_get_serial_sequence('regional', 'id'), coalesce(max(id), 1)) FROM regional;
 
